10db0b63edae1ff8162af40855acaf4a599b91d3
Chmouss <cyprien.lek@hotmail.com>
2025-10-31 22:06:51 +0100

    basic API request get

--- src/main/java/com/cyprien/cooking_recipes/CookingRecipesApplication.java
+++ src/main/java/com/cyprien/cooking_recipes/CookingRecipesApplication.java
@@ -2,12 +2,20 @@ package com.cyprien.cooking_recipes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CookingRecipesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CookingRecipesApplication.class, args);
+        SpringApplication.run(CookingRecipesApplication.class, args);
 	}
+
+    @GetMapping
+    public String helloWorld(){
+        return "Hello world spring boot";
+    }
 
 }
